792f4d06c6ba999a04e7ce5fe9717dd7991a5eab
changedi <jysoftware@gmail.com>
2013-07-02 15:29:09 +0800

    mod
    Signed-off-by: changedi <jysoftware@gmail.com>

--- src/main/java/weka/changedi/AdCSVLoader.java
+++ src/main/java/weka/changedi/AdCSVLoader.java
@@ -4,29 +4,53 @@
 package weka.changedi;
 
 import java.io.File;
-import java.io.IOException;
 
+import weka.core.Instances;
 import weka.core.converters.CSVLoader;
 
 /**
  * @author zunyuan.jy
- *
+ * 
  */
-public class AdCSVLoader {
+public class AdCSVLoader implements Prepare {
 
-	public void run(String filePath) throws Exception{
-		CSVLoader csvLoader = new CSVLoader();
-		csvLoader.setFile(new File(filePath));
-		System.out.println(csvLoader.getDataSet().numInstances());
+	private String filePath;
+
+	/**
+	 * @return the filePath
+	 */
+	public String getFilePath() {
+		return filePath;
 	}
+
+	/**
+	 * @param filePath
+	 *            the filePath to set
+	 */
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		AdCSVLoader loader = new AdCSVLoader();
-		loader.run("D:\\快盘\\projects\\91助手渠道数据新.csv");
+		loader.setFilePath("D:\\快盘\\projects\\91助手渠道数据新.csv");
+		loader.run();
+	}
+
+	@Override
+	public void run() throws Exception {
+		CSVLoader csvLoader = new CSVLoader();
+		csvLoader.setFile(new File(filePath));
+		Instances insts = csvLoader.getDataSet();
+		Thread.sleep(10000);
+		System.gc();
+		Thread.sleep(10000);
+		System.out.println(insts.numInstances());
 	}
 
 }
